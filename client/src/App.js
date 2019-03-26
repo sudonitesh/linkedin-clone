5f96e632b53a3b07f623e90cb85cb9dd25dbf9ce
sudonitesh <nktosni@gmail.com>
2019-03-26 09:54:47 +0530

    profile

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from './components/profile/Profile';
 
 import "./App.css";
 
@@ -51,6 +52,7 @@ class App extends Component {
               <Route path="/register" component={Register} />
               <Route path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -150,6 +150,25 @@ export const getProfiles = () => dispatch => {
     );
 };
 
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 // Clear profile
 export const clearCurrentProfile = () => {
   return {
