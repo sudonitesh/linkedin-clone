952d9c5248d5233f2e7df56b92f12640638407fa
sudonitesh <nktosni@gmail.com>
2019-03-26 09:19:34 +0530

    add education

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import PrivateRoute from "./components/common/PrivateRoute";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 import "./App.css";
 
@@ -70,6 +71,13 @@ class App extends Component {
                   exact
                   path="/add-experience"
                   component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
             </div>
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -73,6 +73,19 @@ export const addExperience = (expData, history) => dispatch => {
     );
 };
 
+// Add education
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    .post("/api/profile/education", eduData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Profile loading
 export const setProfileLoading = () => {
   return {
