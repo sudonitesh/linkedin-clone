4e144cd45f68ccf9be1c781b229691cb8f506944
Nitesh Tosniwal <nktosni@gmail.com>
2019-04-16 17:11:17 +0530

    prod setup

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 const initialState = {};
 
@@ -10,10 +11,11 @@ const store = createStore(
   rootReducer,
   initialState,
   // applyMiddleware(...middleware)
-  compose(
-    applyMiddleware(...middleware), 
-    window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__() 
-  )
+  composeWithDevTools(applyMiddleware(...middleware))
+  // compose(
+  //   applyMiddleware(...middleware), 
+  //   window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__() 
+  // )
 );
 
 export default store;
